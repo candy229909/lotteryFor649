8119ba1d2fd22cfec325d7091b0a449bb6fd564e
candy229909 <52559100+candy229909@users.noreply.github.com>
2024-05-19 10:52:24 +0800

    Update XGBoost.py

--- XGBoost.py
+++ XGBoost.py
@@ -1,22 +1,25 @@
-from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
 
-# Train Decision Tree models for each ball number
-model1 = DecisionTreeClassifier(random_state=42)
+# Reducing the number of estimators further to limit training time
+n_estimators = 20
+
+# Train XGBoost models for each ball number with reduced estimators
+model1 = XGBClassifier(n_estimators=n_estimators, random_state=42)
 model1.fit(X_train1, y_train1)
 
-model2 = DecisionTreeClassifier(random_state=42)
+model2 = XGBClassifier(n_estimators=n_estimators, random_state=42)
 model2.fit(X_train2, y_train2)
 
-model3 = DecisionTreeClassifier(random_state=42)
+model3 = XGBClassifier(n_estimators=n_estimators, random_state=42)
 model3.fit(X_train3, y_train3)
 
-model4 = DecisionTreeClassifier(random_state=42)
+model4 = XGBClassifier(n_estimators=n_estimators, random_state=42)
 model4.fit(X_train4, y_train4)
 
-model5 = DecisionTreeClassifier(random_state=42)
+model5 = XGBClassifier(n_estimators=n_estimators, random_state=42)
 model5.fit(X_train5, y_train5)
 
-model6 = DecisionTreeClassifier(random_state=42)
+model6 = XGBClassifier(n_estimators=n_estimators, random_state=42)
 model6.fit(X_train6, y_train6)
 
 # Predicting the next set of numbers
